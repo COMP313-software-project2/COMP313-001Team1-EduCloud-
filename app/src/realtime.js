82a1073888ae64c6c25239b4878b2c4dc93cef1e
MinJiKim123 <61156520+MinJiKim123@users.noreply.github.com>
2020-12-04 16:57:49 -0500

    file-sharing component new

--- app/src/realtime.js
+++ app/src/realtime.js
@@ -19,78 +19,51 @@ export default class Realtime {
     reconnect(){
 
         const store = this.store;
-
         window.setInterval(()=>{
-
             const user = store.getCurrentUser();
             if(user && !this.isConnected){
-
                 console.log("try reconnecting...");
-
                 this.connect();
             }
-
         }, 3000)
     }
-
     decodeMessage(msg) {
-
         let message = {};
-
         try {
-
             message = JSON.parse(msg);
-
         }
         catch (err) {
-
             console.log(err);
         }
-
         return message;
     }
 
     readMessage(msg) {
-
         const store = this.store;
         const currentUser = store.getCurrentUser();
         const currentUserId = _.toString(_.get(currentUser, '_id'));
         const message = this.decodeMessage(msg);
-
         const action = _.get(message, 'action', '');
-
         const payload = _.get(message, 'payload');
 
         switch (action) {
-
             case 'user_offline':
-
                 this.onUpdateUserStatus(payload, false);
                 break;
             case 'user_online':
-
                     const isOnline = true;
                     this.onUpdateUserStatus(payload, isOnline);
-
                 break;
             case 'message_added':
-
                     const activeChannel = store.getActiveChannel();
-
                     let notify = _.get(activeChannel, '_id') !== _.get(payload, 'channelId') && currentUserId !== _.get(payload, 'userId');
                     this.onAddMessage(payload, notify);
-
                 break;
-
             case 'channel_added':
-
                 // to do check payload object and insert new channel to store.
                 this.onAddChannel(payload);
-
                 break;
-
             default:
-
                 break;
         }
 
@@ -98,37 +71,23 @@ export default class Realtime {
     }
 
     onUpdateUserStatus(userId, isOnline = false){
-
         const store = this.store;
-
-
         store.users = store.users.update(userId, (user) => {
-
-
            if(user){
                user.online = isOnline;
-
            }
-
             return user;
-
         });
-
         store.update()
-
     }
     onAddMessage(payload, notify = false){
-
+        console.log("INSIDE ONADDMESSAGE")
         const store = this.store;
         const currentUser = store.getCurrentUser();
         const currentUserId = _.toString(_.get(currentUser, '_id'));
-
         let user = _.get(payload, 'user');
-
-
         // add user to cache
         user = store.addUserToCache(user);
-
         const messageObject = {
             _id: payload._id,
             body: _.get(payload, 'body', ''),
@@ -137,27 +96,18 @@ export default class Realtime {
             created: _.get(payload, 'created', new Date()),
             me: currentUserId === _.toString(_.get(payload, 'userId')),
             user: user,
-
+            type: _.get(payload,'type','string'),
+            filetype: _.get(payload,'filetype','')
         };
-
-
-
-        
-
         store.setMessage(messageObject, notify);
-
     }
 
     onAddChannel(payload) {
-
+        console.log("INSIDE ONADDCHANEL")
         const store = this.store;
-
         const channelId = _.toString(_.get(payload, '_id'));
         const userId = `${payload.userId}`;
-
         const users = _.get(payload, 'users', []);
-
-
         let channel = {
             _id: channelId,
             title: _.get(payload, 'title', ''),
@@ -167,115 +117,64 @@ export default class Realtime {
             messages: new OrderedMap(),
             userId: userId,
             created: new Date(),
-            
-
         };
-
         _.each(users, (user) => {
-
             // add this user to store.users collection
-
             const memberId = `${user._id}`;
-
             this.store.addUserToCache(user);
-
             channel.members = channel.members.set(memberId, true);
-
-
         });
-
-
-
         const channelMessages = store.messages.filter((m) => _.toString(m.channelId)=== channelId);
-
         channelMessages.forEach((msg) => {
-
             const msgId = _.toString(_.get(msg, '_id'));
             channel.messages = channel.messages.set(msgId, true);
-
         })
-
-
         store.addChannel(channelId, channel);
-
     }
 
     send(msg = {}) {
-
         const isConnected = this.isConnected;
-
         if (this.ws && isConnected) {
-
             const msgString = JSON.stringify(msg);
-
             this.ws.send(msgString);
         }
-
     }
-
+    
     authentication() {
         const store = this.store;
-
         const tokenId = store.getUserTokenId();
-
         if (tokenId) {
-
             const message = {
                 action: 'auth',
                 payload: `${tokenId}`
             }
-
             this.send(message);
         }
-
     }
 
 
     connect() {
-
         //console.log("Begin connecting to server via websocket.");
-
         const ws = new WebSocket(websocketUrl);
         this.ws = ws;
-
-
         ws.onopen = () => {
-
-
             //console.log("You are connected");
-
             // let tell to the server who are you ?
-
             this.isConnected = true;
-
             this.authentication();
-
-
             ws.onmessage = (event) => {
-
                 this.readMessage(_.get(event, 'data'));
-
-
                 console.log("Mesage from the server: ", event.data);
             }
-
-
         }
-
         ws.onclose = () => {
-
             //console.log("You disconnected!!!");
             this.isConnected = false;
             //this.store.update();
-
         }
-
         ws.onerror = () => {
-
             this.isConnected = false;
             this.store.update();
         }
-
-
     }
 }
